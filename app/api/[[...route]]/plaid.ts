c8dc0c7c69ff2603fc1d5f475de32dbbd7327c7f
Aidan McKay <aidanmckay2000@gmail.com>
2024-09-20 17:23:43 -0500

    Exchange public token api call and db update

--- app/api/[[...route]]/plaid.ts
+++ app/api/[[...route]]/plaid.ts
@@ -8,6 +8,10 @@ import { CountryCode, PlaidEnvironments, Products } from 'plaid';
 
 import { z } from 'zod';
 import { zValidator } from '@hono/zod-validator';
+import { db } from '@/db/drizzle';
+import { connectedBanks } from '@/db/schema';
+
+import { createId } from '@paralleldrive/cuid2';
 
 
 const app = new Hono()
@@ -54,6 +58,43 @@ const app = new Hono()
 
     return c.json({ 
       data: tokenResponse.data.link_token
+    }, 200);
+  }
+)
+.post(
+  "/exchange-public-token",
+  clerkMiddleware(),
+  zValidator(
+    "json",
+    z.object({
+      publicToken: z.string()
+    })
+  ),
+  async (c) => {
+    const auth = getAuth(c);
+
+    const { publicToken } = c.req.valid('json');
+
+    if(!auth?.userId) {
+      return c.json({ error: "Unauthorized"}, 400);
+    };
+
+    const accessToken = await plaidClient.itemPublicTokenExchange({
+      public_token: publicToken,
+    });
+
+    const [ connectedBank ] = await db
+    .insert(connectedBanks)
+    .values({
+      userId: auth.userId,
+      accessToken: accessToken.data.access_token,
+      id: createId(),
+    })
+    .returning();
+
+    
+    return c.json({ 
+      data: accessToken.data.access_token
     }, 200);
   }
 )
