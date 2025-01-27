65b712ce69d84ffd6776832f1538ab85be3bc09e
Aidan McKay <aidanmckay2000@gmail.com>
2025-01-27 10:31:01 -0600

    restructured data fetching to better support streaming

--- components/dashboard/dashboard-view.tsx
+++ components/dashboard/dashboard-view.tsx
@@ -9,56 +9,74 @@ import { RecentTransactions } from '@/components/recent-transactions'
 import { SalesOverview } from '@/components/sales-overview'
 import { ExpensesOverview } from '@/components/expenses-overview'
 import { QuickActions } from '@/components/quick-actions'
-import { DashboardSkeleton } from './dashboard-skeleton';
 import { 
     useGetDashboardSales,
     useGetDashboardBanking,
     useGetDashboardAccounts,
 } from '@/features/summary/api/use-get-summary';
+import { DashboardSkeleton } from './dashboard-skeleton';
 
 export const DashboardView = () => {
     const isMobile = useMediaQuery('(max-width: 768px)');
-    const { data: salesOverview, isLoading: salesLoading } = useGetDashboardSales();
-    const { data: bankingOverview, isLoading: bankingLoading } = useGetDashboardBanking();
-    const { data: accountsOverview, isLoading: accountsLoading } = useGetDashboardAccounts();
 
-    const accounts = accountsOverview?.accounts;
-    const recentTransactions = accountsOverview?.recentTransactions;
-  
-    if(!salesOverview || !bankingOverview || !accountsOverview) return (
-        <div>
-            <h1>No data available</h1>
-        </div>
-    );
-  
     return (
-        <Suspense fallback={<DashboardSkeleton />}>
-            <motion.div
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-                className="grid gap-6 w-full px-4 md:px-10 -mt-24"
-            >
-                <AnimatePresence mode="wait">
-                    <motion.div
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        exit={{ opacity: 0 }}
-                    >
-                        <QuickActions />
-                        <div className="space-y-6">
-                            <OverviewStats bankingOverview={bankingOverview?.banking} salesOverview={salesOverview}/>
-                            <div className={`grid gap-6 ${isMobile ? '' : 'md:grid-cols-2'} w-full`}>
-                                <AccountBalances accounts={accounts} />
-                                <RecentTransactions recentTransactions={recentTransactions} />
-                            </div>
-                            <div className={`grid gap-6 ${isMobile ? '' : 'md:grid-cols-2'} w-full`}>
-                                {/* <ExpensesOverview /> */}
-                            </div>
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className="grid gap-6 w-full px-4 md:px-10 -mt-24"
+        >
+            <AnimatePresence mode="wait">
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                >
+                    {/* <QuickActions /> */}
+                    <div className="space-y-6">
+                        <Suspense fallback={<OverviewStats.Skeleton />}>
+                            <OverviewStatsWrapper />
+                        </Suspense>
+                        
+                        <div className={`grid gap-6 ${isMobile ? '' : 'md:grid-cols-2'} w-full`}>
+                            <Suspense fallback={<AccountBalances.Skeleton />}>
+                                <AccountBalancesWrapper />
+                            </Suspense>
+                            
+                            <Suspense fallback={<RecentTransactions.Skeleton />}>
+                                <RecentTransactionsWrapper />
+                            </Suspense>
                         </div>
-                    </motion.div>
-                </AnimatePresence>
-            </motion.div>
-        </Suspense>
+                        
+                        <div className={`grid gap-6 ${isMobile ? '' : 'md:grid-cols-2'} w-full`}>
+                            {/* <ExpensesOverview /> */}
+                        </div>
+                    </div>
+                </motion.div>
+            </AnimatePresence>
+        </motion.div>
     );
+};
+
+const OverviewStatsWrapper = () => {
+    const { data: salesOverview } = useGetDashboardSales();
+    const { data: bankingOverview } = useGetDashboardBanking();
+    
+    if(!salesOverview || !bankingOverview) return <OverviewStats.Skeleton />
+    return (
+        <OverviewStats 
+            bankingOverview={bankingOverview?.banking} 
+            salesOverview={salesOverview}
+        />
+    );
+};
+
+const AccountBalancesWrapper = () => {
+    const { data: accountsOverview } = useGetDashboardAccounts();
+    return <AccountBalances accounts={accountsOverview?.accounts} />;
+};
+
+const RecentTransactionsWrapper = () => {
+    const { data: accountsOverview } = useGetDashboardAccounts();
+    return <RecentTransactions recentTransactions={accountsOverview?.recentTransactions} />;
 };
